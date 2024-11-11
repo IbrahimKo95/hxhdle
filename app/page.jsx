8067225bf00fd2b22007ea20146df554e8959f18
IbrahimKo95 <ibrahkonate95@gmail.com>
2024-11-11 02:10:38 +0100

    add: change color

--- app/page.jsx
+++ app/page.jsx
@@ -3,17 +3,22 @@ import {useEffect, useState} from "react";
 import characters from "./data/characters.json";
 
 export default function Home() {
-    const [selectedcharacter, setSelectedCharacter] = useState(null);
+    const [selectedCharacter, setSelectedCharacter] = useState(null);
     const [triedCharacters, setTriedCharacters] = useState(null);
     const [choiceList, setChoiceList] = useState(null);
     useEffect(() => {
         setSelectedCharacter(characters[parseInt(String(Math.random() * characters.length))]);
         }, []);
 
-    async function tryCharacter(e) {
+    async function tryCharacter(e, selected) {
         e.preventDefault();
-        setTriedCharacters(triedCharacters !== null ? [...triedCharacters, choiceList[0]] : [choiceList[0]]);
-        setChoiceList(null);
+        if (selected) {
+            setTriedCharacters(triedCharacters !== null ? [...triedCharacters, selected] : [selected]);
+            setChoiceList(null);
+        } else {
+            setTriedCharacters(triedCharacters !== null ? [...triedCharacters, choiceList[0]] : [choiceList[0]]);
+            setChoiceList(null);
+        }
     }
 
     async function searchCharacter(e) {
@@ -32,10 +37,10 @@ export default function Home() {
     }
 
     return (
-        <div>
-            {selectedcharacter && (
-                <div key={selectedcharacter.name}>
-                    <h1>{selectedcharacter.name}</h1>
+        <div className="flex flex-col items-center">
+            {selectedCharacter && (
+                <div key={selectedCharacter.name}>
+                    <h1>{selectedCharacter.name}</h1>
                 </div>
             )}
 
@@ -47,7 +52,7 @@ export default function Home() {
                         </div>
                         <div className="bg-gray-900 flex flex-col w-full max-h-52 overflow-y-scroll">
                             {choiceList && choiceList.map((character) => (
-                                <button key={character.name} className="flex justify-between py-2 px-2">
+                                <button onClick={(e) => (tryCharacter(e, character))} key={character.name} className="flex justify-between py-2 px-2">
                                     <div>{character.name}</div>
                                 </button>
                             ))}
@@ -56,11 +61,28 @@ export default function Home() {
                 </div>
             </div>
 
-            <div>
+            <div className="flex flex-col gap-y-3 w-2/4">
                 {triedCharacters && triedCharacters.map((character) => (
-                    <button key={character.name} className="flex justify-between py-2 px-2">
-                        <div>{character.name}</div>
-                    </button>
+                    <div key={character.name} className="flex justify-between py-2 px-2">
+                        <div>
+                            <p>{character.name}</p>
+                        </div>
+                        <div className={character.gender === selectedCharacter.gender ? "bg-green-500" : "bg-red-500"}>
+                            <p>{character.gender}</p>
+                        </div>
+                        <div className={character.species === selectedCharacter.species ? "bg-green-500" : "bg-red-500"}>
+                            <p>{character.species}</p>
+                        </div>
+                        <div className={character.affiliations === selectedCharacter.affiliations ? "bg-green-500" : "bg-red-500"}>
+                            <p>{character.affiliations}</p>
+                        </div>
+                        <div className={character.nen_type === selectedCharacter.nen_type ? "bg-green-500" : "bg-red-500"}>
+                            <p>{character.nen_type}</p>
+                        </div>
+                        <div className={character.arc === selectedCharacter.arc ? "bg-green-500" : "bg-red-500"}>
+                            <p>{character.arc}</p>
+                        </div>
+                    </div>
                 ))}
             </div>
 
